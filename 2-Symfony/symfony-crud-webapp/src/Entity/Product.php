0fdefb1b22017fbb28111a90376dbd96da340da1
patdem <patrycjadembinska60@gmail.com>
2022-03-30 21:45:35 +0200

    Add symfony task

---
+++ 2-Symfony/symfony-crud-webapp/src/Entity/Product.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $name;
+
+    #[ORM\Column(type: 'integer')]
+    private $categoryId;
+
+    #[ORM\Column(type: 'integer')]
+    private $subcategoryId;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCategoryId(): ?int
+    {
+        return $this->categoryId;
+    }
+
+    public function setCategoryId(int $categoryId): self
+    {
+        $this->categoryId = $categoryId;
+
+        return $this;
+    }
+
+    public function getSubcategoryId(): ?int
+    {
+        return $this->subcategoryId;
+    }
+
+    public function setSubcategoryId(int $subcategoryId): self
+    {
+        $this->subcategoryId = $subcategoryId;
+
+        return $this;
+    }
+}
